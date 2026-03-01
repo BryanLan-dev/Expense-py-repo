61cc00c20cee6bb9d0448a6fe2c6bb1e504227a9
BryanLan-dev <2006landers@gmail.com>
2026-03-01 16:47:21 -0600

    Starting setting up storage file to read data from json and store expense file. Need to  convert Expense objects → dicts write dicts to JSN and read dicts from JSON convert dicts → Expense objects return list of objects.

--- storage.py
+++ storage.py
@@ -1,12 +1,27 @@
-#should read data from data.json
-#should write data to data.json
-#data base manager class
 
-#should load existing expenses
-#save new expenses
-#convert json -> expense objects
-#convert expense objects -> json
-#job is strictly storage 
+import json
+import os
 
-def storage():
-    pass
+class StorageManager:
+    
+    def __init__(self, filename="data.json"):
+        self.filename = filename
+        if not os.path.exists(self.filename):
+            with open(self.filename, "w") as f:
+                json.dump([], f) 
+    
+    def load_expenses(self):
+        with open(self.filename, "r") as f:
+            data = json.load(f)
+            
+
+
+    def save_expenses(self, expenses):
+        with open(self.filename, "w") as f:
+            json.dump(expenses, f)
+
+    
+    def add_expense(self, expense):
+        expenses = self.load_expenses()
+        expenses.append(expense)
+        self.save_expenses(expenses)
